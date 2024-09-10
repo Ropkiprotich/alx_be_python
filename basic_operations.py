f3a0314b9c941a0b3426f8b48b84c1c99fee62e6
Rop <ropkiprotich6@gmail.com>
2024-09-10 13:29:08 -0700

    Arthimetic operations

--- basic_operations.py
+++ basic_operations.py
@@ -1,4 +1,7 @@
 number1 = 10
 number2 = 5
 sum = number1 + number2
-print(sum)
+difference = number1 - number2
+multiplication = number1 * number2
+division = number1 / number2
+print(f"The sum is :{sum}\n The difference is : {difference}\n The multiplication is : {multiplication}\n The division is: {division}")
