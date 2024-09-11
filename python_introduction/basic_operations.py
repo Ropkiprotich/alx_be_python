4815d4cc05855837f43ee21433339328a4ec7991
Rop <ropkiprotich6@gmail.com>
2024-09-11 02:20:00 -0700

    arthmetic python correction

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,4 +4,7 @@ sum = number1 + number2
 difference = number1 - number2
 multiplication = number1 * number2
 division = number1 / number2
-print(f"The sum is :{sum}\n The difference is : {difference}\n The multiplication is : {multiplication}\n The division is: {division}")
+print(f"The sum is :{sum}")
+print(f"The difference is : {difference}")
+print(f" The multiplication is : {multiplication}")
+print(f"The division is: {division}")
