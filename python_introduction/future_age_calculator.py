cd7614b26a43430b418ac3dabc58fb96ef34cb03
Rop <ropkiprotich6@gmail.com>
2024-09-12 05:32:03 -0700

    python intro projects

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,3 @@
+i = int(input( "How old are you"))
+future_age = i + 27
+print(f"In 2050, you will be {future_age} years old")
---
+++ python_introduction/simple interest.py
@@ -0,0 +1,7 @@
+# A program to calculate simple interest
+principal =100
+rate = 0.05
+time =3
+
+i=principal * rate * time 
+print("The simple interest is:", i)
