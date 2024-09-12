e15fee63bc69adca9a6887eacf95b24210b35201
Rop <ropkiprotich6@gmail.com>
2024-09-12 14:20:57 -0700

    file corrections

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 # A program to calculate simple interest
-principal =100
+principal =1000
 rate = 0.05
-time =3
+time = 3
 
-interest=principal * rate * time 
+interest = principal * rate * time 
 print("The simple interest is:", interest)
