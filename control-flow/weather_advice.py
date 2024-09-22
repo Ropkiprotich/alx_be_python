885921c3cd42456be5772a00c5421e9c8af37e1c
Rop <ropkiprotich6@gmail.com>
2024-09-22 12:35:25 -0700

    corrected a file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,10 @@
-weather_conditions1 = "sunny"
-weather_conditions2 = "rainy"
-weather_conditions3 = "cold"
 
 current_weather = input("What's the weather like today? (sunny/rainy/cold):")
-if current_weather == weather_conditions1:
-    weather_conditions = "sunny"
+if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-elif current_weather == weather_conditions2 :
+elif current_weather == "rainy" :
     print("Don't forget your umbrella and a raincoat.")
-elif current_weather == weather_conditions3:
+elif current_weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
