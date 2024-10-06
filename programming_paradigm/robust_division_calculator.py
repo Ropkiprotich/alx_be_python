151dd16eb072741f685a38503ca6021df3aa58f4
Rop <ropkiprotich6@gmail.com>
2024-10-06 13:44:48 -0700

    OOP and exception handling corrections!

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -8,7 +8,7 @@ def safe_divide(numerator, denominator):
         return "Error: Cannot divide by zero."
     
     except ValueError:
-        return "Enter numeric numeric values only"
+        return "Error: Please enter numeric values only."
 def main():
     numerator = input("Enter the numerator")
     denominator = input("Enter the denominator")
