20be873a138aaebcc3a7c6fc8b86881701ec93d2
Rop <ropkiprotich6@gmail.com>
2024-10-13 08:06:00 -0700

    Modification of library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -2,16 +2,24 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
+    def __str__(self):
+         return f" {self.title} by {self.author}"
 class EBook(Book):
 
     def __init__(self, title, author,page_size:int):
         super().__init__(title, author)
         self.page_size = page_size
 
+    def __str__(self):
+         return f" {self.title} by {self.author}, File Size: {self.page_size}"
+
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count =page_count
+        
+    def __str__(self):
+        return f" {self.title} by {self.author}, Page Count: {self.page_count}"
 
 class Library:
     def __init__(self):
